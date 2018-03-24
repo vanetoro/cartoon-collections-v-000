c5e3ef802a285d32212a7d763aab1210b38c6a01
Vanessa Toro <vanetoro84@gmail.com>
2018-03-24 19:21:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end 
 end
 
 def find_the_cheese# code an argument here
